c01588b446750a3b522308568e5cb96bdadf4e24
lpdswing <lpdswing@qq.com>
2019-03-20 11:09:16 +0800

    初始化app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,29 @@
 #-*- coding:utf-8 -*-
 # datetime: 2019/3/20 9:21
+import os
+from flask import Flask, request
+from .config import config
+
+
+
+
+
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    # 处理全局Http请求头,允许跨域
+    # 这里使用Cors插件更方便
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        if request.method == 'OPTIONS':
+            response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+            headers = request.headers.get('Access-Control-Request-Headers')
+            if headers:
+                response.headers['Access-Control-Allow-Headers'] = headers
+        return response
+    return app
