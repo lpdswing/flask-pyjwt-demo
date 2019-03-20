6f2bff0e2322bed138b7088ca4fe6ccfda63aff5
lpdswing <lpdswing@qq.com>
2019-03-20 13:43:44 +0800

    add common file

--- app/common.py
+++ app/common.py
@@ -1,2 +1,16 @@
 #-*- coding:utf-8 -*-
 # datetime: 2019/3/20 9:22
+def trueReturn(data, msg):
+    return {
+        'status': 'success',
+        'data': data,
+        'msg': msg
+    }
+
+
+def falseReturn(data, msg):
+    return {
+        'status': 'error',
+        'data': data,
+        'msg': msg
+    }
