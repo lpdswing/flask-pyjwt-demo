ed7ad6f6193ce69101d0b63f73397525b1b4b9cf
lpdswing <lpdswing@qq.com>
2019-03-20 11:09:50 +0800

    更新config

--- app/config.py
+++ app/config.py
@@ -1,2 +1,38 @@
 #-*- coding:utf-8 -*-
 # datetime: 2019/3/20 9:22
+import os
+import sys
+
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+
+class BaseConfig(object):
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you will never geuss')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = prefix + os.path.join(basedir, 'data-dev.db')
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
